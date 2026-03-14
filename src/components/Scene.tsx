66c8f490ce8af2640a4b790eb83cb0684deedfd0
Zaidan Maarouf <zmaarouf1@etf.unsa.ba>
2026-03-14 01:08:59 +0100

    better light scene

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -247,9 +247,10 @@ function SceneContent({
 
   return (
     <>
-      <ambientLight intensity={0.6} />
-      <directionalLight position={[10, 10, 5]} intensity={1} />
-      <directionalLight position={[-5, 5, -5]} intensity={0.3} />
+      <ambientLight intensity={0.8} />
+      <directionalLight position={[10, 10, 5]} intensity={1.5} castShadow />
+      <directionalLight position={[-5, 8, -5]} intensity={0.5} />
+      <hemisphereLight args={["#ffffff", "#444444", 0.6]} />
 
       <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.01, 0]}>
         <planeGeometry args={[20, 20]} />
